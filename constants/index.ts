2dec1a325717e39fc33a8c02f63e6be3328442aa
Anubhav Pal <anubhavpal.excl@gmail.com>
2024-08-31 16:35:59 +0530

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,64 @@
+export const headingsArray = [
+  {
+    heading: "Conceptualize Your Idea",
+    subheading:
+      "Define the problem your SaaS product will solve and identify your target audience.",
+    ctaText: "Start Ideation",
+  },
+  {
+    heading: "Market Research",
+    subheading:
+      "Analyze competitors, gather feedback, and validate your product concept.",
+    ctaText: "Conduct Research",
+  },
+  {
+    heading: "Design Your Solution",
+    subheading:
+      "Create wireframes, design user interfaces, and plan user experiences.",
+    ctaText: "Design Now",
+  },
+  {
+    heading: "Develop the Product",
+    subheading:
+      "Build your application, focusing on core features, architecture, and scalability.",
+    ctaText: "Start Development",
+  },
+  {
+    heading: "Test and Iterate",
+    subheading:
+      "Conduct thorough testing, gather user feedback, and make improvements.",
+    ctaText: "Test Product",
+  },
+  {
+    heading: "Launch Your Product",
+    subheading:
+      "Prepare for launch with marketing strategies, onboarding processes, and support.",
+    ctaText: "Launch Now",
+  },
+  {
+    heading: "Monitor and Support",
+    subheading:
+      "Track performance, address user issues, and provide ongoing support.",
+    ctaText: "Monitor Performance",
+  },
+  {
+    heading: "Scale and Grow",
+    subheading:
+      "Optimize and scale your SaaS product based on user feedback and market demand.",
+    ctaText: "Grow Your Product",
+  },
+  {
+    heading: "",
+  },
+];
+
+export const colors: string[] = [
+  "#fff",
+  "#fff0f4",
+  "#fff9f0",
+  "#f0f7ff",
+  "#f8fff0",
+  "#f0f2ff",
+  "#fdfff0",
+  "#f0fcff",
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+"use client";
+import React, { useCallback, useEffect, useRef } from "react";
+import { colors, headingsArray } from "../../constants";
 
 const MainComponent = () => {
-  return <div>MainComponent</div>;
+  const cardRefs = useRef<HTMLDivElement[]>([]);
+  const userScrollFunction = useCallback(() => {
+    const threshold = 100;
+    cardRefs.current.map((cardRef, index) => {
+      const cardTop = cardRef.getBoundingClientRect().top;
+      if (cardTop <= threshold + index * 10) {
+        cardRef.style.position = "sticky";
+        cardRef.style.top = `${threshold + index * 10}px`;
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      requestAnimationFrame(userScrollFunction);
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col h-full w-full items-center justify-center gap-2 pt-[100.9px] bg-[#FAFAFA] scroll-smooth	 px-3 md:px-0">
+      {headingsArray.map((box, index) => {
+        return box.heading !== "" ? (
+          <div
+            ref={(ref) => {
+              if (ref) {
+                cardRefs.current[index] = ref;
+              }
+            }}
+            key={box.heading}
+            className="flex items-center justify-center text-2xl rounded-3xl border-[1px]  border-zinc-300 h-[300px] md:h-[450px] w-full md:w-4/5 xl:w-2/3 md:mx-0 transition-transform scroll-smooth	 duration-300 px-3 md:px-0"
+            style={{
+              zIndex: index,
+              backgroundColor: colors[index],
+            }}
+          >
+            <div className="flex flex-col items-center justify-between text-center gap-10">
+              <div className="flex flex-col gap-2">
+                <div className="text-3xl font-semibold">{box.heading}</div>
+                <div className="text-sm font-normal text-gray-600">
+                  {box.subheading}
+                </div>
+              </div>
+              <div className="text-sm rounded-full px-4 py-2 bg-black text-white cursor-pointer">
+                {box.ctaText}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center text-2xl rounded-md h-[450px] w-2/3"></div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default MainComponent;
